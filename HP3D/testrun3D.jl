b96c5596fd18dab81fe599b6c4a111cbb7bbdd8b
Diego RG <unamdiego@ciencias.unam.mx>
2021-06-25 21:29:36 -0500

    Made progress in WL and storing data

--- HP3D/testrun3D.jl
+++ HP3D/testrun3D.jl
@@ -9,12 +9,7 @@ plotly()
 testProteinfcc=Protein([[7 2 9];[7 3 9];[7 4 9];[7 5 9];[7 6 9];[7 7 9];[7 8 9];[7 9 9];[7 10 9];[7 11 9];[7 12 9];[7 13 9];[7 14 9]],
 [1,-1,-1,1,-1,1,-1,1,-1,-1,1,-1,-1],fcc)
 
-infofcc=mainHP3Dmet(20,100,0.1,3,21,testProteinfcc) # Employ metropolis to simulate the protein folding process across a range of temperatures.
-# Save the generated states.
+# Employ metropolis to simulate the protein folding process across a range of temperatures.
 
-writedlm("/Users/pedroruiz/Desktop/Diego/PF/Data/HPMet3D1_1.",infofcc[1])
-writedlm("/Users/pedroruiz/Desktop/Diego/PF/Data/HPMet3D1_2.",infofcc[2])
-writedlm("/Users/pedroruiz/Desktop/Diego/PF/Data/HPMet3D1_3.",infofcc[3])
+# mainHP3Dmet(20,5,0.1,3,11,testProteinfcc) "test"
 
-
-visHP3D(infofcc[end],testProteinfcc.HPlist,20,testProteinfcc.geometry)
